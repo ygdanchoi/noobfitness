cd6da191607ef27103a6fa23e72f2c669446f36b
Dan Choi <ygdanchoi@gmail.com>
2016-12-26 14:40:54 -0500

    Made ExerciseAdapter look more like official code example

--- app/src/main/java/com/noobfitness/noobfitness/MainActivity.java
+++ app/src/main/java/com/noobfitness/noobfitness/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         fourDayDefault.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(v.getContext(), RoutineActivity.class);
+                Intent i = new Intent(v.getContext(), RoutinesActivity.class);
                 startActivity(i);
             }
         });
